7cdd036e8a15d093ea4552b5760457e16e9ec915
suprabhasahu-SDET <suprabhasahu2309@gmail.com>
2026-02-18 12:04:31 +0530

    Reducing number of variables

--- master.py
+++ master.py
@@ -1,12 +1,11 @@
 Total = 10000
-#Effort = 4
-Effort = int(input("what is your daily effort? "))
+Effort = 3
 
 Days = Total // Effort
 Rem_hrs = Total % Effort
 Months = Days // 30
-Rem_days = Days % 30
+Days = Days % 30
 Years = Months // 12
-Rem_months = Months % 12
+Months = Months % 12
 
-print(Years, "yrs", Rem_months, "mth", Rem_days, "dys", Rem_hrs, "hrs")
+print(Years, "yrs", Months, "mth", Days, "dys", Rem_hrs, "hrs")
